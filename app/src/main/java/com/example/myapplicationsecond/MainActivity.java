4b7281e17714989c37799c1fcc3dc1ceae81056d
Каменец <nkamenets23@gmail.com>
2020-03-11 17:56:58 +0700

    prosto tak

--- app/src/main/java/com/example/myapplicationsecond/MainActivity.java
+++ app/src/main/java/com/example/myapplicationsecond/MainActivity.java
@@ -2,14 +2,25 @@ package com.example.myapplicationsecond;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private View img_1;
+    private View img_2;
+    private View img_3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img_1 = findViewById(R.id.imageView);
+        img_2 = findViewById(R.id.imageView2);
+        img_3 = findViewById(R.id.imageView3);
+    }
 
+    public void onClickStart(View view) {
+        img_1.setBackgroundColor(Color.GREEN);
     }
 }
